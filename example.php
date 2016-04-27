25f23b59cd8b271b43966c4e36428730c442d2a8
korenevskiy <koreshs@mail.ru>
2016-04-27 14:35:33 +0300

    Added files via upload

---
+++ example.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html> 
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div>
+<?php 
+    require_once dirname(__FILE__) . '/tth.php';
+    
+    $file = 'index.html';
+    echo "TTH for $file:  ";
+    echo TTH::getTTH($file).'<BR>';
+    
+    $file = 'readme.md';
+    echo "TTH for $file:  ";
+    echo TTH::getTTH($file).'<BR>';
+?>
+        </div>
+    </body>
+</html>
